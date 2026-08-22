f2b91583707ebaa0d50517e5fd421fc34a80d2c5
gabrealinc <hello@gabrealinc.com>
2026-08-21 18:38:52 -0700

    Add mentorship to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["", "/offers", "/retreats", "/schedule", "/about", "/contact"];
+  const routes = ["", "/offers", "/retreats", "/schedule", "/about", "/contact", "/mentorship"];
 
   return routes.map((route) => ({
     url: `https://elevatewithkait.com${route}`,
